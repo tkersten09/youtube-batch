73b4face2b11e30986ed6698bba0539ad8e4d5e1
tkersten09 <34385640+tkersten09@users.noreply.github.com>
2017-12-31 20:23:19 +0100

    Add: Missing functionality of the options. Now all shown options in the help menue are working. And added a expressiv example.

--- pavement.py
+++ pavement.py
@@ -3,25 +3,22 @@ import paver.doctools
 from paver.setuputils import setup
 from pathlib import Path
 
-
 setup(
+    include_package_data=True,
     name="youtube-batch",
-    packages=['youtube_batch/',],
+    packages=['youtube_batch/'],
     version="0.1.0",
     author="Thomas Kersten",
     author_email="tkersten09@gmail.com",
     install_requires=[
-        'youtube_upload',
-        'google-api-python-client',
-        'progressbar2'
-        ],
-    scripts= ["bin/youtube-batch"],
+        'youtube-upload', 'google-api-python-client', 'progressbar2'
+    ],
+    scripts=["bin/youtube-batch"],
     entry_points={
-        'console_scripts': [
-            'youtube-batch = youtube_batch.main:run'
-        ],
+        'console_scripts': ['youtube-batch = youtube_batch.main:run'],
     },
 )
+
 
 @task
 @needs('generate_setup', 'minilib')
@@ -29,11 +26,10 @@ def makeSetup():
     """Overrides sdist to make sure that our setup.py is generated."""
     #eggdir = Path('THz.egg-info')
     #eggdir.rmtree()
-    
+
     pass
 
 
 # the pass that follows is to work around a weird bug. It looks like
 # you can't compile a Python module that ends in a comment.
 pass
-
--- youtube_batch/main.py
+++ youtube_batch/main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # Upload videos to Youtube from the command-line using APIv3.
 #
 # Author: Arnau Sanchez <pyarnau@gmail.com>
@@ -7,20 +7,25 @@
 """
 Upload all videos in given folder to Youtube from the command-line which.
 
-    $ youtube-batch --endingse="mp4, mpg" \
-                     --description="Anne Sophie Mutter plays Beethoven" \
-                     --category=Music \
-                     --tags="mutter, beethoven" \
-                     anne_sophie_mutter.flv
+    youtube-batch --endings="mp4, mpg, mkv"\
+                  --privacy="private"\
+                  --recursive\
+                  --publish-at="2017-01-14T12:00:00+01:00"
+                  D:\Filme C:\File
 """
 
 import os
 import sys
 from pathlib import Path
 import optparse
+import itertools
+# from pprint import pprint as print
 from youtube_upload import lib
 
-class OptionsError(Exception): pass
+
+class OptionsError(Exception):
+    pass
+
 
 EXIT_CODES = {
     OptionsError: 2,
@@ -28,96 +33,172 @@ EXIT_CODES = {
 }
 
 
-def upload_files(file_list):
+def upload_files(file_list, options, args):
     """Upload all files in file_list by using tokland/youtube-upload script."""
     for file in file_list:
-        #Using the youtube-upload. And an example of the used command line is:
-        # youtube-upload --title="Anne Sophie Mutter plays Beethoven" --chunksize="65536" "Anne Sophie Mutter plays Beethoven.mp4"
-        string = 'youtube-upload --title="' + str(file.stem) + '" '
-        
-        additional_options = '" --chunksize="'+ str(options.chunksize) +'" --privacy="private" "' + str(file)  + '"'
-        #print(string)
-        os.system(string)
-        
-def parse_options_error(parser, options):
+        cmd = 'youtube-upload'
+
+        # Add command
+        full_cmd = ''
+        full_cmd += cmd
+
+        # Add additional_options
+        additional_options = {
+            '--chunksize': options.chunksize,
+            '--privacy': 'private',
+            '--title': file.stem,
+            '--publish-at': options.publish_at,
+            '--client-secrets': options.client_secrets,
+            '--credentials-file': options.credentials_file
+        }
+        for option_name, option_value in additional_options.items():
+            # Add option only if the value is not empty
+            if (option_value != '' and option_value != None):
+                full_cmd += ' ' + option_name + '="' + str(option_value) + '"'
+
+        # Add bool options
+        bool_options = {'--auth-browser': options.auth_browser}
+        for option_name, option_bool in bool_options.items():
+            if option_bool:
+                full_cmd += ' ' + option_name
+
+        # Add file name
+        full_cmd += ' "' + str(file) + '"'
+
+        # Run command
+        # print(full_cmd)
+        os.system(full_cmd)
+
+
+def parse_options_error(parser, options, args):
     """Check errors in options."""
     required_options = []
     missing = [opt for opt in required_options if not getattr(options, opt)]
     if missing:
         parser.print_usage()
-        msg = "Some required option are missing: {0}".format(", ".join(missing))
+        msg = "Some required option are missing: {0}".format(
+            ", ".join(missing))
         raise OptionsError(msg)
-        
-        
+
+
 def run_main(parser, options, args, output=sys.stdout):
     """Run the main scripts from the parsed options/args.
        And checks if the given folders exists beforehand."""
-    parse_options_error(parser, options)
+    parse_options_error(parser, options, args)
     #args = [r'E:\Digitalisierte Videos\convert\uploadTest']
     #print("args: {}".format(args))
-    
-    #Checks if all folders exists
+
+    # Checks if all folders exists
     for index, path in enumerate(args):
         p = Path(path)
         if not p.exists():
             raise OptionsError("{0} does not exists".format(path))
-    
+
+    # Get Files with the given endings like mkv, m4v, ...
     endings = [str(s.strip()) for s in (options.endings or "").split(",")]
     file_list = []
     for end in endings:
         if options.recursive:
-            p = Path(path).glob('**/*.'+ end)
+            p = Path(path).glob('**/*.' + end)
         else:
-            p = Path(path).glob('*.'+ end)
+            p = Path(path).glob('*.' + end)
         file_list = itertools.chain(file_list, p)
     file_list = sorted(file_list)
+
     if len(file_list) == 0:
-        raise OptionsError("No files in the given folders. Maybe try -r or --recursive for search recursively for videos in the given folders. Like \n youtube-batch -r FOLDER"
-    upload_files(file_list)
-    
+        raise OptionsError(
+            "No files in the given folders. Maybe try -r or --recursive to search recursively for videos in the given folders. Like \n youtube-batch -r FOLDER [FOLDER2 ...]"
+        )
+
+    upload_files(file_list, options, args)
+
+
 def main(arguments):
     """Define the usage and the options. And then parses the given options/args and give this to run_main()."""
-    
+
     usage = """Usage: %prog [OPTIONS] FOLDER [FOLDER2 ...]
 
-    Uploads all videos in the folders to Youtube."""
+Uploads all videos in the folders to Youtube.
+For example with:
+
+    youtube-batch --endings="mp4, mpg, mkv"\
+ --privacy="private" --publish-at="2017-01-14T20:15:00.0+01:00"\
+ D:\Dateien\Filme\yesterday D:/Dateien/Filme/upload"""
     parser = optparse.OptionParser(usage=usage)
-    
+
     #Additional options
-    parser.add_option("-r", "--recursive",  action="store_true", dest="recursive",
-        help='Search recursively for videos in the given folders.' , default=False)
-    parser.add_option('-e', '--endings', dest='endings', type="string",  default = "mp4",
-        help='Video File Endings (separated by commas: "mp4, m4v,...")')
-    parser.add_option('', '--chunksize', dest='chunksize', type="int", 
-        default = 1024*256, help='Update file chunksize')
-    parser.add_option('', '--privacy', dest='privacy', metavar="STRING",
-        default="public", help='Privacy status (public | unlisted | private)')
-    parser.add_option('', '--publish-at', dest='publish_at', metavar="datetime",
-       default=None, help='Publish date (ISO 8601): YYYY-MM-DDThh:mm:ss.sZ')
-       
+    parser.add_option(
+        "-r",
+        "--recursive",
+        action="store_true",
+        dest="recursive",
+        help='Search recursively for videos in the given folders.',
+        default=False)
+    parser.add_option(
+        '-e',
+        '--endings',
+        dest='endings',
+        type="string",
+        default="mp4",
+        help='Video File Endings (separated by commas: "mp4, m4v,...") [mp4]')
+    parser.add_option(
+        '',
+        '--chunksize',
+        dest='chunksize',
+        type="int",
+        default=1024 * 256,
+        help='Update file chunksize [262144]')
+    parser.add_option(
+        '',
+        '--privacy',
+        dest='privacy',
+        metavar="STRING",
+        default="public",
+        help='Privacy status (public | unlisted | private) [public]')
+    parser.add_option(
+        '',
+        '--publish-at',
+        dest='publish_at',
+        metavar="datetime",
+        default=None,
+        help='Publish date (ISO 8601): YYYY-MM-DDThh:mm:ss.sZ')
+
     # Authentication
-    parser.add_option('', '--client-secrets', dest='client_secrets',
-        type="string", help='Client secrets JSON file')
-    parser.add_option('', '--credentials-file', dest='credentials_file',
-        type="string", help='Credentials JSON file')
-    parser.add_option('', '--auth-browser', dest='auth_browser', action='store_true',
+    parser.add_option(
+        '',
+        '--client-secrets',
+        dest='client_secrets',
+        default='./.client_secrets.json',
+        type="string",
+        help='Client secrets JSON file [./.client_secrets.json]')
+    parser.add_option(
+        '',
+        '--credentials-file',
+        dest='credentials_file',
+        default='',
+        type="string",
+        help='Credentials JSON file')
+    parser.add_option(
+        '',
+        '--auth-browser',
+        dest='auth_browser',
+        default=False,
+        action='store_true',
         help='Open a GUI browser to authenticate if required')
 
-    
     #Fixes bug for the .exe in windows: The help will be displayed, when no arguments are given.
     if len(arguments) == 0:
         parser.print_help()
         return 0
-    
+
     options, args = parser.parse_args(arguments)
-    
-    #print(options)
-    
+
     run_main(parser, options, args)
 
 
 def run():
     sys.exit(lib.catch_exceptions(EXIT_CODES, main, sys.argv[1:]))
-  
+
+
 if __name__ == '__main__':
     run()
